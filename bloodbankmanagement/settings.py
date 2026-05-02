a025a50b7470cc8c180ec5583943c3de1fad4162
vidyasonawane533-lab <vidyasonawane533@gmail.com>
2026-05-03 01:38:39 +0530

    remove sensitive data

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -134,8 +134,8 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'viduu786@gmail.com' # this email will be used to send emails
-EMAIL_HOST_PASSWORD = 'bvea kxrn udgm ispr' # host email password required
+EMAIL_HOST_USER = 'your_email@gmail.com' # this email will be used to send emails
+EMAIL_HOST_PASSWORD = 'your_email_password' # host email password required
 # now sign in with your host gmail account in your browser
 # open following link and turn it ON
 # https://myaccount.google.com/lesssecureapps
