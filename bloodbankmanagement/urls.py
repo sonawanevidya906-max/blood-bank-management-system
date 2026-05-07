3249d29c805c222ece5035b56205e5ef176af428
Vidya Sonawane <sonawanevidya906@gmail.com>
2026-05-07 22:26:27 +0530

    Update urls.py

--- bloodbankmanagement/urls.py
+++ bloodbankmanagement/urls.py
@@ -18,6 +18,8 @@ from django.urls import path,include
 from django.urls import path
 from django.contrib.auth.views import LogoutView,LoginView
 from blood import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns = [
     path('admin/', admin.site.urls),
